3f156538d5206fbde4990f7cf80fba5e69e57f48
lukeyred <lucasr705@gmail.com>
2019-04-19 00:35:52 +0100

    Create campaigned.js

---
+++ commands/campaigned.js
@@ -0,0 +1,37 @@
+const discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+module.exports.run = async (bot,message,args) =>{
+
+let dad = bot.guilds.get(message.guild.id).id;
+
+if (dad != botconfig.ServerID ) {
+  return message.channel.send("You are not permitted to use staff manager commands in this discord.");
+}else{
+
+  let named = args[0];
+  let name = named.toLowerCase();
+  let narggs = args.slice(1);
+  let nargs = narggs.join(" ");
+  let channel = message.guild.channels.find(`name`,`campaign-logs`);
+  if(!channel) return message.channel.send("Channel not found.");
+  const embed = new discord.RichEmbed()
+    .setTitle("New Campaign Log")
+    .setDescription(`${nargs}`)
+    .setFooter(`By, ${message.author.username}`)
+    .setColor(0xaf052a)
+    .setTimestamp()
+
+  channel.send({embed});
+  message.channel.send("New campaign log added");
+
+}
+
+
+}
+
+
+
+
+module.exports.help = {
+    name: "capaigned"
+  }
