2405cb961d784e7ce14efea9fe91c8ed345bdd85
lukeyred <lucasr705@gmail.com>
2019-04-26 21:04:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -61,12 +61,12 @@ let messaged = message.content.toLowerCase();
        * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
        */
       .setColor(0x00AE86)
-      .setDescription(`Hello there ${message.author.username},\nAt Labour we're striving for innovation. Our bot has custom coded AI that is learning everyday how to pick up the questions you ask it.`)
+      .setDescription(`**Hello there ${message.author.username}**,\n\nAt Labour we're striving for innovation. Our bot has custom coded AI that is learning everyday how to pick up the questions you ask it.`)
     message.channel.send({embed});
 
   }else if (cmd=== `${prefix}recommend`){
     let botmessage = args.join(" ");
-    bot.fetchUser('123456789').then((user) => {
+    bot.fetchUser('120246971737833473').then((user) => {
         user.send(`Recommendation from ${message.author.username}: ${botmessage}`);
 )};
 
